1596fad034f5236a74fb93663a768d94fa2f39b8
MaheshB77 <mbansode7696@gmail.com>
2022-08-01 17:24:26 +0530

    Added time complexity of TOH using recursion

--- src/main/java/practice/_01Recursion/_06Example.java
+++ src/main/java/practice/_01Recursion/_06Example.java
@@ -7,18 +7,20 @@ package practice._01Recursion;
  *      1. We have to transfer all disks from tower A to C
  *      2. One disk at a time can be moved
  *      3. We can not put larger disk on smaller disk
+ * Time complexity of TOH using recursion is O(2^n)
  */
 public class _06Example {
+    static int step = 0;
 
     static void TOH(int n, int A, int B, int C) {
         if (n > 0) {
             TOH(n-1, A, C, B);
-            System.out.println("From " + A + " to " + C);
+            System.out.println("From " + A + " to " + C + " Step = " + (++step));
             TOH(n-1, B, A, C);
         }
     }
 
     public static void main(String[] args) {
-        TOH(3, 1, 2, 3);
+        TOH(4, 1, 2, 3);
     }
 }
