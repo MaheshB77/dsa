d857f0896071831300091e0189cbb2334f104bd8
MaheshB77 <mbansode7696@gmail.com>
2024-01-25 01:36:09 +0530

    LinkedList : Updated the implementation

--- src/main/java/concepts/_03LinkedList/LinkedList.java
+++ src/main/java/concepts/_03LinkedList/LinkedList.java
@@ -1,66 +1,51 @@
 package concepts._03LinkedList;
 
-public class LinkedList<T> {
+public class LinkedList {
 
-    public T data;
-    public LinkedList<T> next;
+    Node head;
 
-    LinkedList(T data) {
-        this.data = data;
-        this.next = null;
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
 
-    public void add(T data) {
-        if (this.next == null) {
-            this.next = new LinkedList<>(data);
+    void insert(int data) {
+        Node newNode = new Node(data);
+        if (this.head == null) {
+            head = newNode;
         } else {
-            LinkedList<T> tail = this;
-            while (tail.next != null) {
-                tail = tail.next;
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
             }
-            tail.next = new LinkedList<>(data);
+            last.next = newNode;
         }
     }
 
-    // Traverse the Linked List
-    public void display() {
-        LinkedList<T> tail = this;
-        while (tail.next != null) {
-            System.out.println("data : " + tail.data + ", next : " + tail.next);
-            tail = tail.next;
+    void display() {
+        Node node = head;
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
         }
-        // Tail
-        System.out.println("data : " + tail.data + ", next : " + tail.next);
-    }
-
-    // Recursive way to traverse the Linked List
-    public void display(LinkedList<T> head) {
-        if (head.next != null) {
-            System.out.println("data : " + head.data + ", next : " + head.next);
-            display(head.next);
-        } else {
-            // Tail
-            System.out.println("data : " + head.data + ", next : " + head.next);
+        System.out.println("Data : " + node.data + ", Next : " + node.next);
+        while (node.next != null) {
+            node = node.next;
+            System.out.println("Data : " + node.data + ", Next : " + node.next);
         }
-    }
-
-    // Counting the nodes in the linked list
-    public int size() {
-        int size = 1;
-        LinkedList<T> head = this;
-        while (head.next != null) {
-            head = head.next;
-            size++;
-        }
-        return size;
     }
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>(10);
-        list.add(20);
-        list.add(30);
+        LinkedList list = new LinkedList();
         list.display();
-//        list.display(list);
-        System.out.println(list.size());
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.display();
     }
 }
